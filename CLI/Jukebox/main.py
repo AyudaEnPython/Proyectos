1a7b47268fa07e089b60fe0e0630acb1b7b0cc34
leugimkm <leugim911@hotmail.com>
2021-11-26 02:34:55 -0500

    update

--- CLI/Jukebox/main.py
+++ CLI/Jukebox/main.py
@@ -1,17 +1,39 @@
-from prototools import ProtoDB, Menu
-from prototools.menu import Item
-
+from time import sleep
+from prototools import ProtoDB, Menu, FunctionItem, textbox, progressbar
+from prototools.colorize import *
 
 data = ProtoDB("songs")
 ALBUM = data.get_data()
 
 
+def play(song):
+    print()
+    textbox(
+        yellow(f"{song['title']} - {song['artist']}"),
+        align="center",
+        light=False,
+        bcolor="magenta",
+        width=58,
+        ml=2,
+    )
+    for _ in progressbar(
+        range(int(song['duration'])*10),
+        width=52,
+        units=False,
+    ):
+        sleep(0.1)
+
+
 def main():
-    menu = Menu("Jukebox")
+    menu = Menu(yellow("Jukebox"))
     menu.set_dimension(width=60, height=20)
     menu.builder._header.show_bottom = True
     for song in ALBUM:
-        menu.add_item(Item(f"({song['duration']:.2f}) {song['title']} {song['artist']}"))
+        menu.add_item(FunctionItem(
+            f"({song['duration']:.2f}) {song['title']} {song['artist']}",
+            play,
+            args=[song]
+        ))
     menu.run()
 
 
