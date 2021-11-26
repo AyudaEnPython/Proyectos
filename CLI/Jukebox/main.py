6cee1be94cf5be45ccf9bdab6ab1c99012abf057
leugimkm <leugim911@hotmail.com>
2021-11-25 22:05:28 -0500

    just front for now

---
+++ CLI/Jukebox/main.py
@@ -0,0 +1,19 @@
+from prototools import ProtoDB, Menu
+from prototools.menu import Item
+
+
+data = ProtoDB("CLI/Jukebox/songs")
+ALBUM = data.get_data()
+
+
+def main():
+    menu = Menu("Jukebox")
+    menu.set_dimension(width=60, height=20)
+    menu.builder._header.show_bottom = True
+    for song in ALBUM:
+        menu.add_item(Item(f"({song['duration']:.2f}) {song['title']} {song['artist']}"))
+    menu.run()
+
+
+if __name__ == "__main__":
+    main()
