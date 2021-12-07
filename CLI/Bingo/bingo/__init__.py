ff61c0df96117e4605d5c085ec27069db7e643ce
leugimkm <leugim911@hotmail.com>
2021-12-07 01:50:39 -0500

    added payment methods

--- CLI/Bingo/bingo/__init__.py
+++ CLI/Bingo/bingo/__init__.py
@@ -1,8 +1,10 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
 from prototools import RangeDict
+from typing import List, Dict, Tuple, Optional
 
 PRECIO: int = 8
+METODOS: Tuple[str] = ("VISA", "Transferencia BCP", "Yape")
 RANGOS = ((1, 15), (16, 30), (31, 45), (46, 60), (61, 75))
 INDEX = RangeDict({
     k:v for k, v in zip(
