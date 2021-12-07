8090b3d2b233d172a8c96d500b92f17fe434b9f8
leugimkm <leugim911@hotmail.com>
2021-12-07 12:40:27 -0500

    removed dependencies and minor changes

--- CLI/Bingo/bingo/utils.py
+++ CLI/Bingo/bingo/utils.py
@@ -6,8 +6,6 @@ from typing import Tuple
 # pip install prototools
 from prototools import int_input, menu_input, EMAIL_REGEX
 
-from . import PRECIO, METODOS
-
 
 class DniException(Exception):
     """Excepción para DNI"""
@@ -65,13 +63,15 @@ def validar_email(s: str) -> str:
             continue
 
 
-def agregar_usuario() -> Tuple[str, str, str, int, str]:
+def agregar_usuario(
+    metodos: Tuple[str, str, str] , costo: str,
+) -> Tuple[str, str, str, int, str]:
     nombre = input("Ingrese su nombre y apellido: ")
     dni = validar_dni("Ingrese su DNI: ")
     email = validar_email("Ingrese su email: ")
     n = int_input("Ingrese el número de cartones que desea comprar: ")
-    print(f"Costo Total: S/.{n*PRECIO}")
-    pago = menu_input(METODOS, numbers=True)
+    print(f"Costo Total: S/.{n*costo}")
+    pago = menu_input(metodos, numbers=True)
     return nombre, dni, email, n, pago
 
 
