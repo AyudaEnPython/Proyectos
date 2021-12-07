046fa359e911360e1ec39076b3818411aecdbea8
leugimkm <leugim911@hotmail.com>
2021-12-07 12:41:00 -0500

    change functionality

--- CLI/Bingo/main.py
+++ CLI/Bingo/main.py
@@ -1,48 +1,62 @@
 """AyudaEnPython: https://www.facebook.com/groups/ayudapython
 """
 # pip install prototools
+from typing import Tuple
 from prototools import Menu
 from prototools.colorize import magenta, cyan, yellow
 
 from bingo.bingo import Game
-from bingo.utils import agregar_usuario, datenow
-from bingo import PRECIO
+from bingo.utils import agregar_usuario, modificar_informacion, datenow
+from bingo import LUGAR, FECHA, HORA, COSTO, METODOS
 
 
 class App:
 
-    def __init__(self) -> None:
+    def __init__(self, lugar: str, fecha: str, hora: str, costo: str) -> None:
         self.game = Game()
-        self.configurar()
+        self._lugar = lugar
+        self._fecha = fecha
+        self._hora = hora
+        self._costo = costo
+        self._total: int = 0
+        self._configurar()
 
-    def comprar(self):
-        self.game.add_user(agregar_usuario())
+    def comprar(self) -> None:
+        self.game.add_user(agregar_usuario(METODOS, self._costo))
         self._fecha_hora()
         print("Compra realizada correctamente")
 
-    def visualizar(self):
+    def visualizar(self) -> None:
         print(f"Usuarios: {len(self.game.usuarios)}")
         print(f"{self.game.usuarios}")
-    
-    def configurar(self):
-        self.lugar = "Comunidad Ayuda En Python"
-        self._fecha = "24 de diciembre del 2021"
-        self._hora = "19:00"
+
+    def informacion(self, obj) -> None:
+        self._configurar(*modificar_informacion())
+        obj.subtitle = self.lugar
+        obj.prologue_text = self.info
+
+    def _configurar(
+        self, lugar: str, fecha: str, hora: str, costo: str
+    ) -> None:
+        self._lugar = lugar
+        self._fecha = fecha
+        self._hora = hora
+        self._costo = costo
         self.info = (
             f"{f'{self._fecha}':^26}"
-            f" {f'{self._hora}':^26} {f'S/.{PRECIO}':^26}"
+            f" {f'{self._hora}':^26} {f'S/.{self._costo}':^26}"
         )
 
-    def _fecha_hora(self):
+    def _fecha_hora(self) -> None:
         self._fecha = datenow("%d/%m/%Y")
         self._hora = datenow("%H:%M")
 
-    def run(self):
+    def run(self) -> None:
         self.game.start()
 
 
 def main():
-    app = App()
+    app = App(LUGAR, FECHA, HORA, COSTO)
     app.game.add_user(("Bot", "11001100", "bot@py.com", 1, "VISA"))
     menu = Menu(
         cyan("BINGO"),
@@ -53,8 +67,8 @@ def main():
     )
     menu.add_options(
         ("Comprar cartones", app.comprar),
-        ("Visualizar compras", app.visualizar),
-        ("Configurar", lambda: print()),
+        ("Visualizar compras", app.visualizar), # TODO
+        ("Configurar", app.informacion, menu),
         ("Jugar BINGO", app.run),
     )
     menu.settings(
