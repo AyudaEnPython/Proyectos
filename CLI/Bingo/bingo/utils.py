e9a2435798e00c99b05a688a12b7320b134ba941
leugimkm <leugim911@hotmail.com>
2021-12-07 01:30:31 -0500

    added dni validation

--- CLI/Bingo/bingo/utils.py
+++ CLI/Bingo/bingo/utils.py
@@ -8,13 +8,40 @@ from prototools.inputs import int_input, menu_input
 METODOS = ("VISA", "Transferencia BCP", "Yape")
 
 
+class DniException(Exception):
+    """Excepción para DNI"""
+
+
 def datenow(s: str) -> str:
     return datetime.now().strftime(s)
 
 
+# Adaptado de:
+# https://github.com/AyudaEnPython/Soluciones/blob/main/soluciones/persona_dni.py
+def validar_dni(s: str) -> bool:
+    """Valida un DNI
+
+    :param s: Mensaje de indicación
+    :type s: str
+    :return: DNI validado
+    :rtype: str
+    """
+    while True:
+        try:
+            dni = input(s)
+            if len(dni) != 8:
+                raise DniException("El DNI debe tener 8 caracteres")
+            if not dni.isdigit():
+                raise DniException("El DNI debe contener solo números")
+            return dni
+        except DniException as e:
+            print(e)
+            continue
+
+
 def agregar_usuario() -> Tuple[str, str, str, str]:
     nombre = input("Ingrese su nombre y apellido: ")
-    dni = input("Ingrese su DNI: ")
+    dni = validar_dni("Ingrese su DNI: ")
     email = input("Ingrese su email: ")
     n = int_input("Ingrese el número de cartones que desea comprar: ")
     print(f"Costo Total: S/.{n*5}")
