1378f8032cac894f4fbd43f60dc9248b8fa4f3b9
leugimkm <leugim911@hotmail.com>
2022-09-21 20:28:54 -0500

    u: added menu

--- GUI/ordenamientos/main.py
+++ GUI/ordenamientos/main.py
@@ -1,11 +1,19 @@
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, Frame
 
 
 class CustomCanvas(Canvas):
 
     def __init__(self):
         super().__init__()
+    
+    def draw_data(self):
+        pass
 
+
+class Menu(Frame):
+
+    def __init__(self):
+        super().__init__()
 
 
 class App(Tk):
