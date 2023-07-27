ce0d9e8d63a85e7947561c658587f9f0345d4483
leugimkm <leugim911@hotmail.com>
2023-07-26 21:48:11 -0500

    update

--- GUI/README.md
+++ GUI/README.md
@@ -3,3 +3,4 @@
 - [Calculadora](https://github.com/AyudaEnPython/Proyectos/tree/main/GUI/calculadora)
 - [Generar números aleatorios](https://github.com/AyudaEnPython/Proyectos/tree/main/GUI/generar_aleatorios)
 - [Reloj Digital](https://github.com/AyudaEnPython/Proyectos/tree/main/GUI/reloj_digital)
+- [World Clock](https://github.com/AyudaEnPython/Proyectos/tree/main/GUI/world_clock)
---
+++ GUI/world_clock/main.py
@@ -0,0 +1,43 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+"""
+from time import strftime
+from datetime import datetime, timedelta
+from tkinter import Tk, Label
+
+clocks = []
+TIMEZONES = (
+    ("Peru", -5),
+    ("Italy", 2),
+    ("Japan", 9),
+)
+
+
+def get_time(diff):
+    current_time = datetime.utcnow() + timedelta(hours=diff)
+    return current_time.strftime("%H:%M:%S")
+
+
+def update_times():
+    for i, (_, diff) in enumerate(TIMEZONES):
+        clocks[i].config(text=get_time(diff))
+    root.after(1000, update_times)
+
+
+def setup_gui(master):
+    Label(master, text=strftime("%A %d %B %Y"), bg="black", fg="yellow",
+        font=("Courier", 24)).grid(row=0, columnspan=2, padx=5)
+    for i, (country, _) in enumerate(TIMEZONES, 1):
+        Label(master, text=country, bg="black", fg="white",
+            font=("Courier", 18)).grid(row=i, column=0)
+        clock = Label(
+            master, text="", bg="black", fg="cyan", font=("Courier", 18))
+        clock.grid(row=i, column=1)
+        clocks.append(clock)
+
+
+root = Tk()
+root.title("World Clock")
+root["bg"] = "black"
+setup_gui(root)
+update_times()
+root.mainloop()
